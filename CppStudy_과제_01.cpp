f9c658457303cb06ce7bda32bbece555842e808b
DoYunseo <108567536+DoYunseo@users.noreply.github.com>
2022-08-14 20:05:08 +0900

    Add files via upload

---
+++ CppStudy_과제_01.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "My name is µµÀ±¼­ " << endl; 
+
+	unsigned long a;
+	a = 2147483648;
+	cout << a << endl;
+
+	const float PI = 2.14;
+	char ch1 = 64;
+	char ch2 = ch1 + 1;
+	cout << PI << endl;
+	cout << ch1 << endl;
+	cout << ch2 << endl;
+
+	char ch;
+	int X;
+	unsigned short siX;
+	unsigned iX;
+	long liX;
+	long long lliX;
+	cout << "sizeof(ch):" << sizeof ch << endl;
+	cout << "sizeof(X):" << sizeof X << endl;
+	cout << "sizeof(siX):" << sizeof siX << endl;
+	cout << "sizeof(iX):" << sizeof iX << endl;
+	cout << "sizeof(liX):" << sizeof liX << endl;
+	cout << "sizeof(lliX):" << sizeof lliX << endl;
+
+	
+	cout << "Please enter Fahrenheit Value: ";
+	int Fahrenheit;
+	cin >> Fahrenheit;
+	int Celsius;
+	Celsius = 5 / 9 * (Fahrenheit - 32);
+	cout << "Celcius value is " << Celsius << endl;
+
+
+	return 0;
+
+
+
+	}
